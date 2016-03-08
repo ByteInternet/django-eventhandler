db139d6ba7629799fac1e9ba270d6ae5b009356d
Gertjan Oude Lohuis <gertjan@byte.nl>
2016-03-08 11:20:42 +0100

    configure dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='django-eventhandler',
-    version='',
+    version='0.1',
+    provides=['eventhandler'],
     packages=['eventhandler'],
     url='https://github.com/ByteInternet/django-eventhandler',
     license='3-clause BSD',
     author='Gertjan Oude Lohuis',
     author_email='gertjan@byte.nl',
     description='RabbitMQ event handler as a Django module',
-    install_requires=['Django', 'python-events']
+    install_requires=['Django>=1.6', 'python-events==0.1'],
+    dependency_links=['https://github.com/ByteInternet/python-events/tarball/master#egg=python-events-0.1']
 )
