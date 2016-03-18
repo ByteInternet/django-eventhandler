ac0daf9cdec6e31152065805a92f06e2428e24ea
Gertjan Oude Lohuis <gertjan@byte.nl>
2016-03-18 12:32:37 +0100

    Update reqs

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
     ],
     keywords='amqp django rabbitmq events eventhandler',
     description='RabbitMQ event handler as a Django module',
-    install_requires=['Django>=1.6', 'amqpconsumer==1.2'],
+    install_requires=['Django>=1.6', 'amqpconsumer==1.4'],
 )
