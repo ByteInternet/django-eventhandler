6ab6238e0665a0855693a221603ba14c24e22d4c
Gertjan Oude Lohuis <gertjan@byte.nl>
2016-03-30 16:24:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-eventhandler',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(exclude=['tests*']),
     url='https://github.com/ByteInternet/django-eventhandler',
     author='Byte B.V.',
