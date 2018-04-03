ee34a4615ff7febfda7f09ed63df0ff7f3876eff
Bono de Visser <bono@byte.nl>
2018-04-03 15:57:09 +0200

    Bump version for new django support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-eventhandler',
-    version='0.5.2',
+    version='0.5.3',
     packages=find_packages(exclude=['tests*']),
     url='https://github.com/ByteInternet/django-eventhandler',
     author='Byte B.V.',
