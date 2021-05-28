3c2c73937bccfc7445ddc7b1b546503a669754d4
Bono de Visser <bono@byte.nl>
2021-05-28 16:37:48 +0200

    Use latest amqpconsumer to prevent more bugs in later pika versions
    Latest amqpconsumer should finally have all the bugs removed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-eventhandler',
-    version='0.5.8',
+    version='0.5.9',
     packages=find_packages(exclude=['tests*']),
     url='https://github.com/ByteInternet/django-eventhandler',
     author='Byte B.V.',
